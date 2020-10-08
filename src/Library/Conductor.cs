71d72a4f4f36fdf6301da5e2d813fab004290062
Nicolas <nicolas.lozano.282002@gmail.com>
2020-10-08 19:09:02 -0300

    Modificacion Conductor

---
+++ src/Library/Conductor.cs
@@ -0,0 +1,60 @@
+namespace UCURide
+{
+    public class Conductor : Usuario
+    {
+        
+       private int calificacionconductor;
+
+       private string biografia;
+
+       private string vehiculo;
+       
+       private int CantidadCalificaciones = 0;
+       
+       public int CalificacionConductor
+       {
+           get
+           {
+               return this.calificacionconductor;
+           }
+           set
+           {
+               this.calificacionconductor = value;
+           }
+       }
+
+        public string Biografia
+       {
+           get
+           {
+               return this.biografia;
+           }
+           set
+           {
+               this.biografia = value;
+           }
+       }
+       
+              public string Vehiculo
+       {
+           get
+           {
+               return this.vehiculo;
+           }
+           set
+           {
+               this.vehiculo = value;
+           }
+       }
+        public Conductor(string nombre, string apellido)
+        : base(nombre, apellido)
+        {
+        }
+
+        public void CalificarConductor(int calificacion)
+        {
+            CantidadCalificaciones ++;
+            CalificacionConductor = (calificacion + CalificacionConductor*(CantidadCalificaciones-1))/CantidadCalificaciones;
+        }
+    }
+}
